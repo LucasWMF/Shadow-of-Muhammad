8091da677bf5bf4997af9601ba4b6bfdfe173e84
Lucas Wagner de Melo Fogaça <141058064+LucasWMF@users.noreply.github.com>
2024-09-18 00:31:19 -0300

    Adições

---
+++ config/conexao.php
@@ -0,0 +1,7 @@
+<?php
+$dbhost = 'endereco_do_servidor_externo';
+$dbname = 'nome_do_banco';
+$dbusername = 'usuario';
+$dbpassword = 'senha';
+
+?>
--- src/JavaScript/app.js
+++ src/JavaScript/app.js
@@ -11,31 +11,31 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Atualize o caminho estático para refletir a nova estrutura
-app.use(express.static(path.join(__dirname, '../../src')));
+app.use(express.static(path.join(__dirname, '../../public')));
 
 // Atualize os caminhos para enviar os arquivos
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../pages', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../views', 'index.html'));
 });
 
 app.get('/historia', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../pages', 'historia.html'));
+    res.sendFile(path.join(__dirname, '../../views', 'historia.html'));
 });
 
 app.get('/modalidades', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../pages', 'modalidades.html'));
+    res.sendFile(path.join(__dirname, '../../views', 'modalidades.html'));
 });
 
 app.get('/ranking', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../pages', 'ranking.html'));
+    res.sendFile(path.join(__dirname, '../../views', 'ranking.html'));
 });
 
 app.get('/feedback', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../pages', 'feedback.html'));
+    res.sendFile(path.join(__dirname, '../../views', 'feedback.html'));
 });
 
 app.get('/painel', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../pages', 'panel.php'));
+    res.sendFile(path.join(__dirname, '../../views', 'panel.php'));
 });
 
 app.listen(port, function() {
